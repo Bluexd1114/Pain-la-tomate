fea715046468d378c62672d84fb6745afc9e4ab8
Bluexd1114 <107968867+Bluexd1114@users.noreply.github.com>
2022-07-31 14:02:10 -0500

    Add files via upload

---
+++ ....cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x,y,z;
+	cout<<"Este programa sirve para sumar"<<endl;
+	cout<<"Introduzca el primer valor"<<endl;
+	cin>>x;
+	cout<<"Introduzca el segundo valor"<<endl;
+	cin>>y;
+	z=x+y;
+	if(z>20)
+	cout<<"La suma es mayor que 20"<<endl;
+			
+	else 
+	cout<<"La suma es menor que 20"<<endl;
+	
+	return 0;
+}
